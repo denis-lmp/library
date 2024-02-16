4d3a144b8fc9b31ac9f156ae2d134c05a46e363a
dev-denis <denis.kostaev@lamp-dev.com>
2024-02-16 13:33:45 +0100

    Don't need handler, just return 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,13 +28,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-
-        $this->renderable(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => 'Record not found.'
-                ], 404);
-            }
-        });
     }
 }
